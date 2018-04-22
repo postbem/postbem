b468a9927ebd9602dfda4d8faceb512591d902ca
Ruslan Khusnetdinov <ruslankhh@gmail.com>
2018-04-22 14:54:22 +0300

    feat: Add normolize single prop for void elements for parser

--- test/parser/parser.test.js
+++ test/parser/parser.test.js
@@ -12,7 +12,9 @@ describe('parser', () => {
         content: [
           { elem: 'Header', content: 'Parser' },
           { elem: 'Main', content: 'It\'s work!' },
-          'text',
+          'text1',
+          { tag: 'br' },
+          'text2',
           { elem: 'Footer' }
         ]
       }
@@ -22,14 +24,31 @@ describe('parser', () => {
   it('should work', () => {
     const parser = new Parser(bemjson);
     const tree = parser.parse();
-    const result1 = tree.content[0].content[0].content[0].block;
+    const result1 = tree.content[0].content[0].block;
     const result2 = tree.content[0].content[0].content[1].content;
 
     expect(result1).to.be.equal('App');
     expect(result2).to.be.equal('It\'s work!');
   });
 
-  it('should simple match', () => {
+  it('should normalize nodes', () => {
+    const parser = new Parser(bemjson);
+    const tree = parser.parse();
+    let result1;
+    let result2;
+
+    tree.match({ block: 'App', elem: 'Header' }, (node) => {
+      result1 = node.block;
+    });
+    tree.match({ tag: 'br' }, (node) => {
+      result2 = node.single;
+    });
+
+    expect(result1).to.be.equal('App');
+    expect(result2).to.be.equal(true);
+  });
+
+  it('should simple match tree', () => {
     const parser = new Parser(bemjson);
     const tree = parser.parse();
     let result1;
