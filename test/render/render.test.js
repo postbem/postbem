41da551a5c05111158f014de722cf0739763124f
Ruslan Khusnetdinov <ruslankhh@gmail.com>
2018-04-23 01:28:03 +0300

    fix: Refactoring render

--- test/render/render.test.js
+++ test/render/render.test.js
@@ -4,23 +4,25 @@ const { expect } = require('chai');
 const render = require('../../lib/render');
 
 describe('render', () => {
-  const bemtree = {
-    block: 'Page',
-    content: [
-      {
-        block: 'App',
-        content: [
-          { elem: 'Header', content: 'Parser' },
-          { elem: 'Main', content: 'It\'s work!' },
-          null,
-          undefined
-        ]
-      }
-    ]
+  const bemTree = {
+    content: [{
+      block: 'Page',
+      content: [
+        {
+          block: 'App',
+          content: [
+            { elem: 'Header', content: 'Parser' },
+            { elem: 'Main', content: 'It\'s work!' },
+            null,
+            undefined
+          ]
+        }
+      ]
+    }]
   };
 
   it('should work', () => {
-    const tree = render(bemtree);
+    const tree = render(bemTree);
     const result1 = tree.content[0].block;
     const result2 = tree.content[0].content[1].content;
 
@@ -28,14 +30,12 @@ describe('render', () => {
     expect(result2).to.be.equal('It\'s work!');
   });
 
-  it('should clean', () => {
-    const tree = render(bemtree);
-    const expected2 = bemtree.content[0].content
-      .filter(node => !!node).length;
+  it('should normalize', () => {
+    const tree = render(bemTree);
     const result1 = tree.match;
-    const result2 = tree.content[0].content.length;
+    const result2 = tree.content[0].content[3];
 
     expect(result1).to.be.equal(undefined);
-    expect(result2).to.be.equal(expected2);
+    expect(result2).to.be.equal(null);
   });
 });
