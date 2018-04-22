2ba183114447871e0c0f3dd5717827937e879b7e
Ruslan Khusnetdinov <ruslankhh@gmail.com>
2018-04-23 01:36:10 +0300

    feat: Release lib

--- test/index.test.js
+++ test/index.test.js
@@ -2,12 +2,36 @@ const { describe, it } = require('mocha');
 const { expect } = require('chai');
 
 const postbem = require('../lib');
+const plugin = require('./utils/plugin');
 
 describe('postbem', () => {
-  it('should work', () => {
-    const expected = 'It\'s work!';
-    const result = postbem();
+  const bemtree = {
+    block: 'Page',
+    content: [
+      {
+        block: 'App',
+        content: [
+          { elem: 'Header', content: 'Parser' },
+          { elem: 'Main', content: 'It\'s work!' },
+          { elem: 'Footer' }
+        ]
+      }
+    ]
+  };
 
-    expect(result).to.be.equal(expected);
+  it('should work', () => {
+    const processor = postbem([plugin]);
+    const expected1 = bemtree.content[0].content[0].content.toUpperCase();
+    const expected2 = bemtree.content[0].content[1].content.toUpperCase();
+
+    return processor.process(bemtree)
+      .then(result => result.content)
+      .then(tree => {
+        const result1 = tree.content[0].content[0].content;
+        const result2 = tree.content[0].content[1].content;
+
+        expect(result1).to.be.equal(expected1);
+        expect(result2).to.be.equal(expected2);
+      });
   });
 });
